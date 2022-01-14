200c8de3739799033761052720ee943042bc404b
Jan Behrens <jan.behrens@kit.edu>
2022-01-14 12:49:40 +0100

    Improve speech synth

--- speech_synth.cc
+++ speech_synth.cc
@@ -1,6 +1,7 @@
 #include "userosc.h"
 #include "stmlib/dsp/dsp.h"
 #include "stmlib/dsp/limiter.h"
+#include "stmlib/dsp/cosine_oscillator.h"
 #include "plaits/dsp/engine/speech_engine.h"
 #include "plaits/dsp/speech/naive_speech_synth.h"
 #include "plaits/dsp/speech/sam_speech_synth.h"
@@ -8,7 +9,7 @@
 #include "utils/fixed_math.h"
 
 uint16_t p_values[6] = {0};
-float shape = 0, shiftshape = 0, shape_lfo = 0, mix = 0;
+float shape = 0, shiftshape = 0, shape_lfo = 0, lfo2 = 0, mix = 0, blend = 0;
 bool gate = false, previous_gate = false;
 
 plaits::EngineParameters parameters = {
@@ -20,6 +21,8 @@ plaits::EngineParameters parameters = {
     .accent = 0
 };
 
+stmlib::CosineOscillator lfo;
+
 enum LfoTarget {
     LfoTargetShape,
     LfoTargetShiftShape,
@@ -27,10 +30,13 @@ enum LfoTarget {
     LfoTargetParam2,
     LfoTargetPitch,
     LfoTargetAmplitude,
+    LfoTargetLfo2Frequency,
+    LfoTargetLfo2Depth
 };
 
 inline float get_lfo_value(enum LfoTarget target) {
-    return (p_values[k_user_osc_param_id2] == target ? shape_lfo : 0.0f);
+    return (p_values[k_user_osc_param_id3] == target ? shape_lfo : 0.0f) +
+    (p_values[k_user_osc_param_id6] == target ? lfo2 : 0.0f);
 }
 
 inline float get_shape() {
@@ -42,30 +48,26 @@ inline float get_shift_shape() {
 inline float get_param_id1() {
     return clip01f((p_values[k_user_osc_param_id1] * 0.005f) + get_lfo_value(LfoTargetParam1));
 }
-
-#if defined(USE_LIMITER)
-stmlib::Limiter limiter_;
-#endif
+inline float get_param_id2() {
+    return clip01f((p_values[k_user_osc_param_id2] * 0.01f) + get_lfo_value(LfoTargetParam2));
+}
+inline float get_param_lfo2_frequency() {
+    return clip01f((p_values[k_user_osc_param_id4] * 0.01f) + get_lfo_value(LfoTargetLfo2Frequency));
+}
+inline float get_param_lfo2_depth() {
+    return clip01f((p_values[k_user_osc_param_id5] * 0.01f) + get_lfo_value(LfoTargetLfo2Depth));
+}
 
 plaits::NaiveSpeechSynth naive_speech_synth_;
 plaits::SAMSpeechSynth sam_speech_synth_;
 
-//float* temp_buffer_[2];
-
 void OSC_INIT(uint32_t platform, uint32_t api)
 {
-    //stmlib::BufferAllocator allocator;
-    //temp_buffer_[0] = allocator.Allocate<float>(plaits::kMaxBlockSize);
-    //temp_buffer_[1] = allocator.Allocate<float>(plaits::kMaxBlockSize);
-  
+    lfo.InitApproximate(0);
+    lfo.Start();
+    
     sam_speech_synth_.Init();
     naive_speech_synth_.Init();
-
-    #if defined(USE_LIMITER)
-    limiter_.Init();
-    #endif
-
-    p_values[0] = 100;
 }
 
 void OSC_NOTEON(const user_osc_param_t * const params)
@@ -79,13 +81,15 @@ void OSC_NOTEOFF(const user_osc_param_t * const params)
 
 void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn, const uint32_t frames)
 {
-    static float out1[plaits::kMaxBlockSize], out2[plaits::kMaxBlockSize], aux[plaits::kMaxBlockSize];
-    float* temp_buffer_[2];
+    static float out1[plaits::kMaxBlockSize], out2[plaits::kMaxBlockSize];
+    static float aux1[plaits::kMaxBlockSize], aux2[plaits::kMaxBlockSize];
 
     shape_lfo = q31_to_f32(params->shape_lfo);
+    lfo.InitApproximate(get_param_lfo2_frequency() / 600.f);
+    lfo2 = (lfo.Next() - 0.5f) * 2.0f * get_param_lfo2_depth();
     
     parameters.note = ((float)(params->pitch >> 8)) + ((params->pitch & 0xFF) * k_note_mod_fscale);
-    //parameters.note += (get_lfo_value(LfoTargetPitch) * 0.5);
+    parameters.note += (get_lfo_value(LfoTargetPitch) * 0.5);
     
     if(gate && !previous_gate) {
         parameters.trigger = plaits::TRIGGER_RISING_EDGE;
@@ -96,7 +100,8 @@ void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn, const uint32_t
     
     parameters.timbre = get_shift_shape();
     parameters.morph = get_shape();
-    mix = get_param_id1();
+    blend = get_param_id1();
+    mix = get_param_id2();
     
     const float f0 = plaits::NoteToFrequency(parameters.note);
     
@@ -106,7 +111,7 @@ void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn, const uint32_t
         parameters.morph,
         parameters.timbre,
         nullptr,
-        aux,
+        aux1,
         out1,
         plaits::kMaxBlockSize);
     
@@ -115,29 +120,15 @@ void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn, const uint32_t
         f0,
         parameters.morph,
         parameters.timbre,
-        aux,
+        aux2,
         out2,
         plaits::kMaxBlockSize);
 
-#if 0    
-    for (size_t i = 0; i < plaits::kMaxBlockSize; ++i) {
-        aux[i] += (temp_buffer_[0][i] - aux[i]) * mix;
-        out[i] += (temp_buffer_[1][i] - out[i]) * mix;
-    }
-    
     for(size_t i=0;i<plaits::kMaxBlockSize;i++) {
-        yn[i] = f32_to_q31(out[i]);
+        float o = stmlib::Crossfade(out1[i], out2[i], blend);
+        float a = stmlib::Crossfade(aux1[i], aux2[i], blend);
+        yn[i] = f32_to_q31(stmlib::Crossfade(o, a, mix));
     }
-#else
-    #if defined(USE_LIMITER)
-    limiter_.Process(1.0, out1, plaits::kMaxBlockSize);
-    limiter_.Process(1.0, out2, plaits::kMaxBlockSize);
-    #endif
-    
-    for(size_t i=0;i<plaits::kMaxBlockSize;i++) {
-        yn[i] = f32_to_q31(stmlib::Crossfade(out1[i], out2[i], mix));
-    }
-#endif
 }
 
 void OSC_PARAM(uint16_t index, uint16_t value)
